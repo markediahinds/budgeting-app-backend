64a906a3a8bcfaacb91e9d59871bde654c40e6e1
Markedia Hinds <mhinds@pursuit.org>
2024-07-08 14:57:00 -0400

    Back-End: 100% Complete, Stretch Goal: randomized ids via uuid package

--- validations/transactionValidations.js
+++ validations/transactionValidations.js
@@ -2,8 +2,8 @@ const checkForNameKey = (req, res, next) => {
     if(req.body.hasOwnProperty('item_name')) { 
         next() 
     } else {
-        res.send({ error: `Not quite, Transactions must include the item's name`})
+        res.json({ error: `Not quite, Transactions must include the item's name`})
     }
 }
 
-module.exports = checkForNameKey
+module.exports = { checkForNameKey }
