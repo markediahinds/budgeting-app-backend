82387bf8edb89404d2d02ef244df0f85f7f174e7
Markedia Hinds <mhinds@pursuit.org>
2024-07-09 07:36:45 -0400

    Install, Import, and Initiate Cors Middleware to accept JSON

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const cors = require('cors')
+
 const transactionsController = require('./controllers/transactionsController')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+
 
 app.use('/transactions', transactionsController)
 
 app.get('/', (req, res) => {
-    res.send('M.O.K.A Presents: Budgtr')
+    res.send('M.O.K.A Presents: Budgt')
 })
 
 
